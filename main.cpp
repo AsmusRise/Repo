b065acc14c153917b764c27c1a2c580e616ec30f
Nikolai Dahl Jensen <nikolaidahljensen@gmail.com~>
2025-02-22 20:17:27 +0100

    1.1

--- main.cpp
+++ main.cpp
@@ -1,28 +1,27 @@
 #include <iostream>
-#include <fstream>
-#include <string>
 #include <vector>
+#include <string>
+#include <fstream>
 
-class Task
-{
-	public:
-	std::string _description;
-	Task(std::string description){
-		_description = description;
-	}
+class Task {
+public:
+    std::string description;
+    Task(std::string desc) : description(desc) {}
 };
 
-class TodoList
-{
-	
-	public:
-	std::vector<Task> todolist;
-	TodoList(){}
+class TodoList {
+public:
+    std::vector<Task> tasks;
+    void addTask(std::string desc) { tasks.push_back(Task(desc)); }
+    void showTasks() {
+        for (auto& task : tasks) std::cout << task.description << "\n";
+    }
 };
 
 int main() {
-
-    TodoList todo = TodoList();
-
+    TodoList todo;
+    todo.addTask("Købe ind");
+    todo.showTasks();
     return 0;
 }
+
