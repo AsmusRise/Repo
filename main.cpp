a649ab0a88de4576edf6fd3d91a6de19a3d658e1
AsmusRise <asmusrise@gmail.com>
2025-02-22 08:32:32 +0100

    Method to add tasks

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 using namespace std;
 //starting main.cpp
-
+//writing method addTask
 class Task
 {
 	public:
@@ -26,11 +26,28 @@ class TodoList
 			cout <<task._description << endl;
 		}
 	}
+
+	//method to add task to todolist
+	void addTask(const Task& task){
+		todolist.push_back(task);
+	}
+
+
 };
 
 int main() {
 
     TodoList todo = TodoList();
+
+    //create tasks
+    Task task1("buy grocceries");
+    Task task2("Complete homework");
+    Task task3("Call the bank");
+
+    //Add tasks to the todolist
+    todo.addTask(task1);
+    todo.addTask(task2);
+    todo.addTask(task3);
 
     //print the todo list
     todo.printTodoList();
