2a9f69147679f26d332e194ffc0c622cbfe6ef7b
Nikolai Dahl Jensen <nikolaidahljensen@gmail.com~>
2025-02-22 20:28:09 +0100

    1.2

--- main.cpp
+++ main.cpp
@@ -12,16 +12,42 @@ public:
 class TodoList {
 public:
     std::vector<Task> tasks;
+
     void addTask(std::string desc) { tasks.push_back(Task(desc)); }
+
     void showTasks() {
-        for (auto& task : tasks) std::cout << task.description << "\n";
+        for (size_t i = 0; i < tasks.size(); ++i) {
+            std::cout << i + 1 << ". " << tasks[i].description << "\n";
+        }
+    }
+
+    void editTask(int index, std::string newDesc) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks[index].description = newDesc;
+        } else {
+            std::cout << "Ugyldigt index!\n";
+        }
     }
 };
 
 int main() {
     TodoList todo;
+
+    // Tilføj opgaver
     todo.addTask("Købe ind");
+    todo.addTask("Læse til eksamen");
+    todo.addTask("Lave mad");
+
+    // Vis alle opgaver
+    std::cout << "Opgaver før redigering:\n";
     todo.showTasks();
+
+    // Rediger en opgave
+    todo.editTask(1, "Købe ind og ryde op");
+
+    // Vis opgaver efter redigering
+    std::cout << "\nOpgaver efter redigering:\n";
+    todo.showTasks();
+
     return 0;
 }
-
